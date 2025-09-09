7cc5a1a144d549ac62edaf0545bee75d78e6c710
tiansongye <tiansongye@rcplatformhk.com>
2025-09-09 20:52:41 +0800

    提交 gas 优化文件

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,11 +1,14 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-contract Contract { 
+contract Contract {
     // Addition function
-    function add(uint256 a, uint256 b) public pure returns (uint256) {
-        return a + b;
+    function add(uint256 a, uint256 b) public pure returns (uint256 c) {
+        assembly {
+            c := add(a, b)
+        }
     }
+
     // Subtraction function
     function sub(uint256 a, uint256 b) public pure returns (uint256) {
         return a - b;
